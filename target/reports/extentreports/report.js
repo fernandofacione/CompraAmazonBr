f574b3432f16bd36b71806b9d12095e8346048a1
Fernando Facione <85024081+fernandofacione@users.noreply.github.com>
2021-10-07 22:25:54 -0300

    Testes

---
+++ target/reports/extentreports/report.js
@@ -0,0 +1,207 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("comprasAmazon.feature");
+formatter.feature({
+  "comments": [
+    {
+      "line": 1,
+      "value": "#language: pt"
+    }
+  ],
+  "line": 2,
+  "name": "Comprar Produto PO",
+  "description": "",
+  "id": "comprar-produto-po",
+  "keyword": "Funcionalidade"
+});
+formatter.before({
+  "duration": 65200,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 1725701600,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "Pesquisar Produto e Incluir no Carrinho",
+  "description": "",
+  "id": "comprar-produto-po;pesquisar-produto-e-incluir-no-carrinho",
+  "type": "scenario",
+  "keyword": "Cenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "que acesso o site da Amazon",
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 5,
+  "name": "pesquiso por \"Cafeteira Single, Vermelho, 110v, Cadence\"",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 6,
+  "name": "clico na Lupa",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 7,
+  "name": "vejo a lista de resultados para o produto \"Cafeteira Single, Vermelho, 110v, Cadence\"",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 8,
+  "name": "clico sobre o produto definido",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 9,
+  "name": "clico adicionar produto no carrinho",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 10,
+  "name": "acesso carrinho de compras",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 11,
+  "name": "confirmo o nome do produto como \"Cafeteira Single, Vermelho, 110v, Cadence\" e o preco de \"R$ 97,79\"",
+  "keyword": "Entao "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.after({
+  "duration": 30100,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 595997700,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 45500,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 1398420200,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 13,
+  "name": "Pesquisar Produto com Enter e Incluir no Carrinho",
+  "description": "",
+  "id": "comprar-produto-po;pesquisar-produto-com-enter-e-incluir-no-carrinho",
+  "type": "scenario",
+  "keyword": "Cenario"
+});
+formatter.step({
+  "line": 14,
+  "name": "que acesso o site da Amazon",
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 15,
+  "name": "pesquiso por \"Cafeteira Single, Vermelho, 110v, Cadence\"",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 16,
+  "name": "pressiono Enter",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 17,
+  "name": "vejo a lista de resultados para o produto \"Cafeteira Single, Vermelho, 110v, Cadence\"",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 18,
+  "name": "clico sobre o produto definido",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 19,
+  "name": "clico adicionar produto no carrinho",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 20,
+  "name": "acesso carrinho de compras",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 21,
+  "name": "confirmo o nome do produto como \"Cafeteira Single, Vermelho, 110v, Cadence\" e o preco de \"R$ 97,79\"",
+  "keyword": "Entao "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.after({
+  "duration": 26200,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 598284700,
+  "status": "passed"
+});
+});
